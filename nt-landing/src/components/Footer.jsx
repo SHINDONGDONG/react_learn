3dad9640ac2dbfbdf77f3530fb2c17a3aad3b1fe
신민철 <homerjay@naver.com>
2025-01-05 19:06:13 +0900

    add : Footer and Signup Page Frotend code

--- nt-landing/src/components/Footer.jsx
+++ nt-landing/src/components/Footer.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import React from "react";
+import { BsFacebook, BsInstagram, BsTiktok, BsTwitter } from "react-icons/bs";
+import logo from "../assets/logo/logo.png";
 
 export const Footer = () => {
+  const links = [
+    {
+      title: "About",
+      data: ["About", "Terms", "Legal"],
+    },
+    {
+      title: "NFT",
+      data: ["OpenSea", "Maker", "Learn"],
+    },
+    {
+      title: "Contact",
+      data: ["Press", "Support"],
+    },
+    {
+      title: "Social",
+      data: ["Twiiter", "Instagram"],
+    },
+  ];
+
+  const socialLink = [
+    <BsFacebook />,
+    <BsTwitter />,
+    <BsInstagram />,
+    <BsTiktok />,
+  ];
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div className="upper">
+        <div className="brand-container">
+          <div className="brand">
+            <img src={logo} alt="logo" />
+          </div>
+          <p>Exclusive NFT Collection</p>
+          <ul>
+            {socialLink.map((link, index) => (
+              <li key={index}>{link}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="links">
+          {links.map(({ title, data }, index) => {
+            return (
+              <div className="link" key={index}>
+                <h4>{title}</h4>
+                <ul>
+                  {data.map((link, index2) => (
+                    <li key={index2}>{link}</li>
+                  ))}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="lower">
+        <span>&copy; Copyright 2025 NFT</span>
+        <span>Launching Auguest 2025</span>
+      </div>
+    </footer>
+  );
+};
--- nt-landing/src/components/Signup.jsx
+++ nt-landing/src/components/Signup.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import signup from "../assets/signup.png";
 
 export const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="signup">
+      <div className="container">
+        <div className="content">
+          <p className="sub-title">Launcing soon</p>
+          <h1 className="title">An NFT like no thoer</h1>
+          <p className="description">
+            Dont iss out on the release of our new NFT
+          </p>
+          <button>Sign up</button>
+        </div>
+        <div className="image-container">
+          <div className="image">
+            <img src={signup} alt="home" />
+          </div>
+          <div className="ellipse-container">
+            <div className="ellipse pink"></div>
+            <div className="ellipse orange"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
