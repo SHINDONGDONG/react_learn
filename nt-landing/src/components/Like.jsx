dcc76a57ef19aca017323e0c4fe43ad6de291d59
신민철 <homerjay@naver.com>
2025-01-05 18:37:21 +0900

    Merge branch 'like_front'

--- nt-landing/src/components/Like.jsx
+++ nt-landing/src/components/Like.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from "react";
+import eth1 from "../assets/eth1.png";
+import eth2 from "../assets/eth2.png";
 
 export const Like = () => {
   return (
-    <div>Like</div>
-  )
-}
+    <div className="like">
+      <div className="container">
+        <div className="content">
+          <div className="image">
+            <img src={eth1} alt="eth1" />
+            <h2 className="title">An NFT like no other</h2>
+            <p className="description">
+              Qui ullamco occaecat irure laboris velit mollit Ullamco magna
+              tempor dolor ad minim reprehenderit officia adipisicing irure ad
+              amet id id.
+            </p>
+            <p className="description">
+              Laboris nisi laboris proident sunt exercitation non cupidatat
+              magna Sunt amet incididunt id ipsum et voluptate.
+            </p>
+          </div>
+        </div>
+        <div className="content">
+          <div className="image">
+            <img src={eth2} alt="eth2" />
+            <h2 className="title">An NFT like no other</h2>
+            <p className="description">
+              Qui ullamco occaecat irure laboris velit mollit Ullamco magna
+              tempor dolor ad minim reprehenderit officia adipisicing irure ad
+              amet id id.
+            </p>
+            <p className="description">
+              Laboris nisi laboris proident sunt exercitation non cupidatat
+              magna Sunt amet incididunt id ipsum et voluptate.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- nt-landing/src/components/Release.jsx
+++ nt-landing/src/components/Release.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { BsArrowRight } from 'react-icons/bs'
-import release1 from "../assets/release1.png"
-import release2 from "../assets/release2.png"
-import { Card } from './Card'
+import React from "react";
+import { BsArrowRight } from "react-icons/bs";
+import release1 from "../assets/release1.png";
+import release2 from "../assets/release2.png";
+import { Card } from "./Card";
 
 export const Release = () => {
   return (
@@ -11,14 +11,19 @@ export const Release = () => {
         <div className="content">
           <h2 className="title">Initial Release 4/11</h2>
           <p className="description">
-          We have released four limited edition NFT's early which which can be bid on via <a href='#'>OpenSea.</a>
+            We have released four limited edition NFT's early which which can be
+            bid on via <a href="#">OpenSea.</a>
           </p>
           <p className="description">
             These NFTs are unique and will not be available again in the future.
           </p>
           <p className="description">
-            Make sure to place your bids early to secure your piece of digital art.</p>
-          <a href="#" className='link'>Check them out <BsArrowRight/></a>
+            Make sure to place your bids early to secure your piece of digital
+            art.
+          </p>
+          <a href="#" className="link">
+            Check them out <BsArrowRight />
+          </a>
         </div>
         <div className="image">
           <img src={release1} alt="release" />
@@ -27,28 +32,34 @@ export const Release = () => {
       </div>
       <div className="release green">
         <div className="card-container">
-        <Card
+          <Card
             image={release2}
             series="Floop Series"
             title="Purple Man"
             price={2.99}
             tag={123894}
-            time={1} />
+            time={1}
+          />
           <div className="ellipse orange"></div>
         </div>
         <div className="content">
           <h2 className="title">Initial Release 4/11</h2>
           <p className="description">
-          We have released four limited edition NFT's early which which can be bid on via <a href='#'>OpenSea.</a>
+            We have released four limited edition NFT's early which which can be
+            bid on via <a href="#">OpenSea.</a>
           </p>
           <p className="description">
             These NFTs are unique and will not be available again in the future.
           </p>
           <p className="description">
-            Make sure to place your bids early to secure your piece of digital art.</p>
-          <a href="#" className='link'>Check them out <BsArrowRight/></a>
+            Make sure to place your bids early to secure your piece of digital
+            art.
+          </p>
+          <a href="#" className="link">
+            Check them out <BsArrowRight />
+          </a>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
