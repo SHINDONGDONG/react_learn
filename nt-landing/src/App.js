fd7b3129d1a23ac9edbf8c46e2de6f21015cfcfc
신민철 <homerjay@naver.com>
2025-01-16 00:02:41 +0900

    add: mediaquery

--- nt-landing/src/App.js
+++ nt-landing/src/App.js
@@ -35,6 +35,8 @@ const App = () => {
   window.setTimeout(() => {
     const home = document.getElementsByClassName("home");
     home[0].style.transform = "none";
+    const nav = document.getElementsByTagName("nav");
+    nav[0].style.transform = "none";
   }, 1500);
 
   return (
